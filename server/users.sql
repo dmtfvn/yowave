8e9f0e0bca22fca19f43764016e9f47c72c5e2d0
dmtfvn <dmtfvn@gmail.com>
2025-08-07 10:47:54 +0300

    Add comments to sql query

--- server/users.sql
+++ server/users.sql
@@ -1,4 +1,11 @@
--- Query for creating a 'users' table to store user account information.
+-- Query for creating a 'users' table in PostgreSQL to store user account information.
+
+-- The table includes columns for:
+--   id: Unique identifier for the user (auto-incremented)
+--   username: User's chosen username (unique, max length 20)
+--   email: User's email address (unique, max length 49)
+--   passhash: The hashed password for the user
+--   userid: A unique identifier for the user, generated using the uuid library
 
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
